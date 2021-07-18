0ec8be9eb85c74b58d73b11fe54a4ee92705e057
Yuna <yunauehara32@gmail.com>
2021-07-18 19:00:50 -0300

    Modifications

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 const form = document.getElementById('form')
 
 
-form.addEventListener('submit', (e) => {
+form.addEventListener("submit", (e) => {
     e.preventDefault();
     let nome = document.getElementById('nome').value;
     let email = document.getElementById('email').value;
