c2664229e44a7abfbcfc06325fad4cf909fcd34a
Yuna <yunauehara32@gmail.com>
2021-07-18 21:49:21 -0300

    Modifications

--- js/script.js
+++ js/script.js
@@ -2,26 +2,26 @@ const form = document.getElementById('form')
 
 
 form.addEventListener("submit", (e) => {
-    e.preventDefault();
-    let nome = document.getElementById('nome').value;
-    let email = document.getElementById('email').value;
-    let data = {
-        nome,
-        email,
-    }
+  e.preventDefault();
+  let nome = document.getElementById('nome').value;
+  let email = document.getElementById('email').value;
+  let data = {
+    nome,
+    email,
+  }
 
-    let convertData = JSON.stringify(data);
+  let convertData = JSON.stringify(data);
 
-    localStorage.setItem('lead', convertData)
+  localStorage.setItem('lead', convertData)
 
-    let content = document.getElementById('content')
+  let content = document.getElementById('content')
 
-    let carregando = `<p> Um momento... </p>`
+  let carregando = `<p> Um momento... </p>`
 
-    let pronto = `<p> Concluído! Obrigado por se cadastrar! </p>`
+  let pronto = `<p> Concluído! Obrigado por se cadastrar! </p>`
 
   content.innerHTML = carregando
-  
+
 
   setTimeout(() => {
     content.innerHTML = pronto
